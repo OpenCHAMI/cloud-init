ddf450fc5d330ff2d26e40d0599a616582ce1a2a
Alex Lovell-Troy <alovelltroy@lanl.gov>
2024-12-18 13:49:07 -0500

    feat: update cluster defaults demo script to show ssh key and base-url

--- demo/set_cluster_defaults.sh
+++ demo/set_cluster_defaults.sh
@@ -3,12 +3,12 @@
 curl -X POST http://localhost:27777/cloud-init/admin/cluster-defaults/ \
     -H "Content-Type: application/json" \
     -d '{
-        "cloud-provider": "openchami",
-        "region": "us-west-2",
-        "availability-zone": "us-west-2a",
-        "cluster-name": "venado",
-        "public-keys": [
-            "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEArV2...",
-            "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEArV3..."
-        ]
-    }'
+          "cloud_provider": "OpenCHAMI",
+          "region": "lanl",
+          "availability-zone": "lanl-yellow",
+          "cluster-name": "venado",
+          "base-url": "http://127.0.0.1:27777",
+	  "public-keys": [
+	      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINsQK8VMZM/BtweT1X6rC+ropQLoFhZ/8wE/miFtWV4f"
+	      ]
+        }'
